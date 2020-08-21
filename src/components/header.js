8b7966879cbe21407310f1c5a42ba096cf985135
rober <me@rober.dev>
2020-08-21 11:29:56 +0200

    Fix change language

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,24 @@
 // Vendor libs
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { i18n, withTranslation, Link } from '../lib/i18n';
 
 // Component definition
 const Header = ({ t }) => {
+  // Component state members
+  const [lng, setLng] = useState('');
+
+  useEffect(() => {
+    if (i18n && i18n.language) {
+      setLng(i18n.language.toLowerCase());
+    }
+  }, [i18n.language]);
+
+  // Event handlers
   const changeLangageHandler = (e, l) => {
     e.preventDefault();
     i18n.changeLanguage(l.toLowerCase());
+    setLng(l.toLowerCase());
   };
 
   return (
@@ -23,7 +34,7 @@ const Header = ({ t }) => {
         </Link>
       </div>
       <div>
-        <span>{i18n.language} - &nbsp;</span>
+        <span>{lng.toUpperCase()} - &nbsp;</span>
         <button type='button' onClick={e => changeLangageHandler(e, 'ES')}>
           ES
         </button>
